56abc9248be2ad2fd726d792a6d0731b2f686e98
p41155a <p41155a@gmail.com>
2022-02-28 14:36:20 +0900

    refactor: APIService enum -> protocol
    - 이름 수정하고 적용을 안해서 오류남

--- CryptocurrencyExchange/CryptocurrencyExchange/Network/Assets/AssetsAPIManager.swift
+++ CryptocurrencyExchange/CryptocurrencyExchange/Network/Assets/AssetsAPIManager.swift
@@ -10,7 +10,7 @@ import Foundation
 class AssetsAPIManager: APIProvider {
     /// 모든 자산 검색
     func fetchAssetsStatus(completion: @escaping (Result<AssetsStatusEntity, APIError>) -> ()) {
-        guard let requestURL = APIService.assetsStatus(orderCurrency: OrderCurrency.all.value).setRequest() else {
+        guard let requestURL = AssetsAPIService.assetsStatus(orderCurrency: OrderCurrency.all.value).setRequest() else {
             completion(.failure(APIError.invalidURL))
             return
         }
@@ -19,7 +19,7 @@ class AssetsAPIManager: APIProvider {
     
     /// 각 자산 검색
     func fetchAssetsStatus(cryptocurrencyName: String, completion: @escaping (Result<AppointedAssetsStatusEntity, APIError>) -> ()) {
-        guard let requestURL = APIService.assetsStatus(orderCurrency: cryptocurrencyName).setRequest() else {
+        guard let requestURL = AssetsAPIService.assetsStatus(orderCurrency: cryptocurrencyName).setRequest() else {
             completion(.failure(APIError.invalidURL))
             return
         }
