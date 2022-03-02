773995b3f1de083c16f7f62ebcd9ccb50af29645
정다연 <ekdus0227@gmail.com>
2022-03-02 14:15:24 +0900

    refactor: DataSet, ZoomFactor 모델 분리
    - DataSet의 UI 설정을 일괄 적용하기 위한 BithumbCandleChartDataSet 모델 생성
    - (데이터의 총 갯수가 몇개이든) default로 보여지는 캔들스틱의 갯수를 정하고 가장 최신 데이터가 보여지도록 zoom factor 모델 생성
    
    #16

---
+++ CryptocurrencyExchange/CryptocurrencyExchange/ChartsByCoin/Chart/Model/ChartZoomFactor.swift
@@ -0,0 +1,15 @@
+//
+//  ChartZoomFactor.swift
+//  CryptocurrencyExchange
+//
+//  Created by 정다연 on 3/2/22.
+//
+
+import UIKit
+
+struct ChartZoomFactor {
+    var scaleX: CGFloat
+    var scaleY: CGFloat
+    var xValue: Double
+    var yValue: Double
+}
