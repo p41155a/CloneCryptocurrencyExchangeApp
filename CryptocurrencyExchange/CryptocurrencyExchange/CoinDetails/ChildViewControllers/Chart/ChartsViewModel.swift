dbd503fc9365a0e7a04fae7c0d91941ebf830a5d
DayeonJung <ekdus0227@gmail.com>
2022-03-03 01:29:22 +0900

    feat: 호가/차트 에서 쓰일 ChildViewController/ViewModel 생성
    #21

---
+++ CryptocurrencyExchange/CryptocurrencyExchange/CoinDetails/ChildViewControllers/Chart/ChartsViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ChartsViewModel.swift
+//  CryptocurrencyExchange
+//
+//  Created by Dayeon Jung on 2022/03/03.
+//
+
+import Foundation
+
+class ChartsViewModel: XIBInformation {
+    var nibName: String?
+    
+    init(nibName: String?) {
+        self.nibName = nibName
+    }
+}
---
+++ CryptocurrencyExchange/CryptocurrencyExchange/CoinDetails/ChildViewControllers/Quote/QuoteViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  QuoteViewModel.swift
+//  CryptocurrencyExchange
+//
+//  Created by Dayeon Jung on 2022/03/02.
+//
+
+import Foundation
+
+class QuoteViewModel: XIBInformation {
+    var nibName: String?
+    
+    init(nibName: String?) {
+        self.nibName = nibName
+    }
+}
