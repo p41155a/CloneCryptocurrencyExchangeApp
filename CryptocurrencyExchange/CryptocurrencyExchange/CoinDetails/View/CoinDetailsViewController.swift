02e54e875e5b920045b1fd1c13a23f6f31125e06
DayeonJung <ekdus0227@gmail.com>
2022-03-03 01:37:09 +0900

    feat: CoinDetailsViewController에서 상단 탭에 연관되는 ChildViewController를 containView로 갖도록 설정
    - 호가/차트/시세 나타낼 스택뷰 상단 탭바 생성
    - 각 탭과 연관된 ChildViewController로 구성되도록 뷰컨트롤러 추가
    - 호가/차트/시세 버튼을 누를 때마다 해당 뷰를 가장 상위로 올려 인터렉션이 가능하게 함
    
    #21

---
+++ CryptocurrencyExchange/CryptocurrencyExchange/CoinDetails/View/CoinDetailsViewController.swift
@@ -0,0 +1,92 @@
+//
+//  CoinDetailsViewController.swift
+//  CryptocurrencyExchange
+//
+//  Created by Dayeon Jung on 2022/03/02.
+//
+
+import UIKit
+
+class CoinDetailsViewController: ViewControllerInjectingViewModel<CoinDetailsViewModel> {
+
+    @IBOutlet weak var topTabBar: UIStackView!
+    
+    var quoteViewController: ViewControllerInjectingViewModel<QuoteViewModel> = {
+        let viewController = QuoteViewController(
+            viewModel: QuoteViewModel(
+                nibName: "QuoteViewController"
+            )
+        )
+        return viewController
+    }()
+    
+    var chartViewController: ViewControllerInjectingViewModel<ChartsViewModel> = {
+        let viewController = ChartViewController(
+            viewModel: ChartsViewModel(
+                nibName: "ChartViewController"
+            )
+        )
+        return viewController
+    }()
+    
+    
+    /// 상단 탭별 연결되는 ViewController를 정의
+    var viewControllerByTab: [CoinDetailsTopTabs: UIViewController] = [:]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setChildViewControllers()
+        setTopTapBarTabEvent()
+    }
+    
+    /// 상단 탭에 연관되는 뷰컨트롤러를 ChildViewController로 설정
+    private func setChildViewControllers() {
+        viewControllerByTab[.quote] = quoteViewController
+        viewControllerByTab[.chart] = chartViewController
+        
+        viewControllerByTab.values.forEach {
+            self.addChildVC($0)
+        }
+    }
+    
+    /// 상단 탭을 누름에 따라 touch 이벤트가 동작하도록 설정
+    private func setTopTapBarTabEvent() {
+        topTabBar.arrangedSubviews.enumerated().forEach { (index, button) in
+            guard let button = button as? UIButton else { return }
+            button.tag = index
+            button.addTarget(
+                self,
+                action: #selector(didTapTab(button:)),
+                for: .touchUpInside
+            )
+        }
+    }
+
+    /// touch 이벤트가 발생할 때마다 누른 탭에 연관되는 view를 최상단 subView로 가져온다
+    @objc func didTapTab(button: UIButton) {
+        guard let tabType = CoinDetailsTopTabs(rawValue: button.tag) else { return }
+        guard let tappedView = viewControllerByTab[tabType]?.view else { return }
+        self.view.bringSubviewToFront(tappedView)
+    }
+    
+    private func addChildVC(_ viewController: UIViewController) {
+        self.addChild(viewController)
+        self.view.addSubview(viewController.view)
+        viewController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            viewController.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            viewController.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            viewController.view.topAnchor.constraint(equalTo: self.topTabBar.bottomAnchor),
+            viewController.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        viewController.didMove(toParent: self)
+    }
+    
+    private func removeChildVC(_ viewController: UIViewController) {
+        viewController.willMove(toParent: nil)
+        viewController.removeFromParent()
+        viewController.view.removeFromSuperview()
+    }
+}
