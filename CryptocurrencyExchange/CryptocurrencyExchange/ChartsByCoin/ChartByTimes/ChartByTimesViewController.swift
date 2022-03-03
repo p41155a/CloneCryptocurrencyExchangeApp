8e492aa00fa590958a9c7594e18970e6a04130fb
정다연 <ekdus0227@gmail.com>
2022-03-03 15:29:45 +0900

    style: 차트를 감쌀 뷰컨트롤러 생성 및 일부 파일 위치 변경

---
+++ CryptocurrencyExchange/CryptocurrencyExchange/ChartsByCoin/ChartByTimes/ChartByTimesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChartByTimesViewController.swift
+//  CryptocurrencyExchange
+//
+//  Created by 정다연 on 3/3/22.
+//
+
+import UIKit
+
+class ChartByTimesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CryptocurrencyExchange/CryptocurrencyExchange/ChartsByCoin/ChartByTimes/ChartByTimesViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ChartByTimesViewModel.swift
+//  CryptocurrencyExchange
+//
+//  Created by 정다연 on 3/3/22.
+//
+
+import Foundation
+
+class ChartByTimesViewModel: XIBInformation {
+    var nibName: String?
+    
+    init(nibName: String?) {
+        self.nibName = nibName
+    }
+}
