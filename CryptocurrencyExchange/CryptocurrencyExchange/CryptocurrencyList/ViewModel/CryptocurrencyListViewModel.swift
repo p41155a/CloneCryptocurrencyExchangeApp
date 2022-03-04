87b69ee392dea22bd272141d1e0c949825e9c8d0
p41155a <p41155a@gmail.com>
2022-03-04 15:18:27 +0900

    feat: 초기 데이터 빠진 부분 넣음
    - 안 맞는 부분이 있어서 안 넣었던 빠진 초기 데이터 넣음
    - (+, -)기호 등등 extension 세부 설정
    
    #13

--- CryptocurrencyExchange/CryptocurrencyExchange/CryptocurrencyList/ViewModel/CryptocurrencyListViewModel.swift
+++ CryptocurrencyExchange/CryptocurrencyExchange/CryptocurrencyList/ViewModel/CryptocurrencyListViewModel.swift
@@ -35,11 +35,11 @@ final class CryptocurrencyListViewModel: XIBInformation {
                 cryptocurrencyData.forEach { data in
                     let currentName = data.key
                     let tickerInfo = data.value
-                    let tableData = CrypotocurrencyListTableViewEntity(symbol: "\(tickerInfo.currentName ?? "")_\(PaymentCurrency.KRW)",
-                                                                       currentPrice: tickerInfo.closingPrice ?? "",
-                                                                       changeRate: "",
-                                                                       changeAmount: "",
-                                                                       transactionAmount: "")
+                    let tableData = self.setTableData(symbol: "\(tickerInfo.currentName ?? "")_\(PaymentCurrency.KRW)",
+                                                 currentPrice: tickerInfo.closingPrice ?? "",
+                                                 changeRate: tickerInfo.fluctateRate24H ?? "",
+                                                 changeAmount: tickerInfo.fluctate24H ?? "",
+                                                 transactionAmount: tickerInfo.accTradeValue ?? "")
                     currencyNameList.append(currentName)
                     tickerKRWList[currentName] = tableData
                 }
@@ -53,26 +53,33 @@ final class CryptocurrencyListViewModel: XIBInformation {
     
     func setWebSocketData(with entity: WebSocketTickerEntity) {
         let tickerInfo = entity.content
-        /// symbol 분리
         let splitedSymbol: [String] = tickerInfo.symbol.split(separator: "_").map { "\($0)" }
         let currentName = splitedSymbol[0]
         let payment = splitedSymbol[1]
-        /// 보여지는 값과 다르게 내려오는 데이터 변환
-        let currentPrice: String = tickerInfo.closePrice.doubleValue?.decimalType ?? tickerInfo.closePrice
-        let changeRate: String = "\(tickerInfo.chgRate.doubleValue?.displayDecimal(to: 2) ?? tickerInfo.chgRate)%"
-        let changeAmount: String = tickerInfo.chgAmt.doubleValue?.decimalType ?? tickerInfo.chgAmt
-        let transactionAmount: String = "\(Int((tickerInfo.value.doubleValue ?? 0) / 1000000).decimalType ?? "")백만"
         
-        let tableData = CrypotocurrencyListTableViewEntity(symbol: "\(currentName)_\(payment)",
-                                                           currentPrice: currentPrice,
-                                                           changeRate: changeRate,
-                                                           changeAmount: changeAmount,
-                                                           transactionAmount: transactionAmount)
+        let tableData = setTableData(symbol: "\(currentName)_\(payment)",
+                                     currentPrice: tickerInfo.closePrice,
+                                     changeRate: tickerInfo.chgRate,
+                                     changeAmount: tickerInfo.chgAmt,
+                                     transactionAmount: tickerInfo.value)
         
         if payment == PaymentCurrency.KRW.value {           /// 지불 방식: KRW
             tickerKRWList.value[currentName] = tableData
         } else if payment == PaymentCurrency.BTC.value {    /// 지불 방식: BTC
             
         }
+    }
+    
+    private func setTableData(symbol: String, currentPrice: String, changeRate: String, changeAmount: String, transactionAmount: String) -> CrypotocurrencyListTableViewEntity {
+        let currentPrice: String = currentPrice.setNumStringForm(isDecimalType: true)
+        let changeRate: String = "\(changeRate.displayDecimal(to: 2).setNumStringForm(isMarkPlusMiuns: true))%"
+        let changeAmount: String = changeAmount.setNumStringForm(isDecimalType: true, isMarkPlusMiuns: true)
+        let transactionAmount: String = "\(Int((transactionAmount.doubleValue ?? 0) / 1000000).decimalType ?? "")백만"
+        
+        return CrypotocurrencyListTableViewEntity(symbol: symbol,
+                                                  currentPrice: currentPrice,
+                                                  changeRate: changeRate,
+                                                  changeAmount: changeAmount,
+                                                  transactionAmount: transactionAmount)
     }
 }
--- CryptocurrencyExchange/CryptocurrencyExchange/Extensions/String+Extension.swift
+++ CryptocurrencyExchange/CryptocurrencyExchange/Extensions/String+Extension.swift
@@ -11,4 +11,27 @@ extension String {
     var doubleValue: Double? {
         return Double(self)
     }
+    
+    func displayDecimal(to place: Int) -> String {
+        guard let doubleValue = doubleValue else {
+            return self
+        }
+        return String(format: "%.\(place)f", doubleValue)
+    }
+    
+    func setNumStringForm(isDecimalType: Bool = false, isMarkPlusMiuns: Bool = false) -> String {
+        guard let doubleValue = doubleValue else {
+            return self
+        }
+        var result: String = self
+        if isDecimalType {
+            let numberFormatter = NumberFormatter()
+            numberFormatter.numberStyle = .decimal
+            result = numberFormatter.string(from: NSNumber(value: doubleValue)) ?? self
+        }
+        if isMarkPlusMiuns {
+            result = doubleValue < 0 ? result : "+\(result)"
+        }
+        return result
+    }
 }
