d7568b80f61478918ac1a6d5749384657efbece7
정다연 <ekdus0227@gmail.com>
2022-03-04 17:30:18 +0900

    feat: Repository 내에 DB에서 캔들스틱 데이터를 받아을 수 있는 메서드 생성
    #17

--- CryptocurrencyExchange/CryptocurrencyExchange/ChartsByCoin/ChartByTimes/Chart/ViewModel/CandleStickRepository.swift
+++ CryptocurrencyExchange/CryptocurrencyExchange/ChartsByCoin/ChartByTimes/Chart/ViewModel/CandleStickRepository.swift
@@ -6,15 +6,22 @@
 //
 
 import Foundation
+import RealmSwift
 
 protocol CandleStickChartRepository {
     func getCandleStickData(parameter: CandleStickParameters, completion: @escaping ([[StickValue]]) -> Void)
+    func candleStickDatas(by timeInterval: TimeIntervalInChart) -> [CandleStickData]?
 }
 
 ///  CandleStickAPIManager의 candleStick 메서드 호출
 ///  메서드 내에서 실제 API 호출
 class ProductionCandleStickRepository: CandleStickChartRepository {
     func getCandleStickData(parameter: CandleStickParameters, completion: @escaping ([[StickValue]]) -> Void) {
+    let realm: Realm
+    
+    init() {
+        self.realm = try! Realm()
+    }
         CandleStickAPIManager().candleStick(
             parameters: parameter
         ) { result in
@@ -25,5 +32,23 @@ class ProductionCandleStickRepository: CandleStickChartRepository {
                     completion([[]])
                 }
             }
+    }
+}
+
+/// DB에 캔들스틱 data 추가/조회/업데이트
+extension ProductionCandleStickRepository {
+    /// DB에 저장되어 있는 캔들스틱 Data 반환
+    func candleStickDatas(by timeInterval: TimeIntervalInChart) -> [CandleStickData]? {
+        let candleSticks = realm.objects(CandleStickByTimeInterval.self)
+        
+        let candleSticByInterval = candleSticks.where {
+            $0.interval == timeInterval
+        }.first
+        
+        guard let candleStickDatas = candleSticByInterval else {
+            return nil
+        }
+        
+        return candleStickDatas.stickDatas.map({$0})
     }
 }
