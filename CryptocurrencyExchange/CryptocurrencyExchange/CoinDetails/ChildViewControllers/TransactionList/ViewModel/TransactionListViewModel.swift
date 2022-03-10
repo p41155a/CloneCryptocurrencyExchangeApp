cbddd79b7a8872468ff9a371823c57da2197ffaa
DayeonJung <ekdus0227@gmail.com>
2022-03-10 18:01:35 +0900

    feat: ViewModel에서 뿌려질 배열 업데이트
    - repository로부터 받아온 informations를 transactionInformations 배열에 업데이트하고, 업데이트할 인덱스를 Observable로 전달

--- CryptocurrencyExchange/CryptocurrencyExchange/CoinDetails/ChildViewControllers/TransactionList/ViewModel/TransactionListViewModel.swift
+++ CryptocurrencyExchange/CryptocurrencyExchange/CoinDetails/ChildViewControllers/TransactionList/ViewModel/TransactionListViewModel.swift
@@ -9,8 +9,50 @@ import Foundation
 
 class TransactionListViewModel: XIBInformation {
     var nibName: String?
+    let paymentCurrency: String
+    let orderCurrency: String
+    let repository: TransactionListRepository
+    let indiceToReload: Observable<[IndexPath]?>
+    var transactionInformations: [TransactionInforamtion]
     
-    init(nibName: String?) {
+    init(nibName: String?,
+         paymentCurrency: String,
+         orderCurrency: String
+    ) {
         self.nibName = nibName
+        self.paymentCurrency = paymentCurrency
+        self.orderCurrency = orderCurrency
+        self.repository = TransactionListRepository(
+            webSocketManager: TransactionWebSocketManager(
+                paymentCurrency: paymentCurrency,
+                orderCurrency: orderCurrency
+            )
+        )
+        self.indiceToReload = Observable(nil)
+        transactionInformations = []
+        self.bindClosures()
+    }
+    
+    private func bindClosures() {
+        repository.transactionInformations.bind { [weak self] informations in
+            guard let infos = informations else { return }
+            infos.map({print($0.date)})
+            let newInformations: [TransactionInforamtion] = infos.reversed()
+            self?.transactionInformations.insert(contentsOf: newInformations, at: 0)
+            self?.indiceToReload.value = newInformations.enumerated().map({ (index, _) -> [IndexPath] in
+                [IndexPath(item: index*3, section: 0), IndexPath(item: index*3 + 1, section: 0), IndexPath(item: index*3 + 2, section: 0)
+                 
+                ]}).flatMap({$0})
+            
+            
+        }
+    }
+    
+    func countOfInformations() -> Int {
+        return transactionInformations.count * 3
+    }
+    
+    func information(with index: Int) -> TransactionInforamtion {
+        return transactionInformations[index]
     }
 }
